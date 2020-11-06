3e4184e26f5fa728ca99606751bfe95a38ce6695
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-11-06 08:57:21 -0500

    Primer controlador

---
+++ src/main/java/co/edu/ucundinamarca/Controller/loginActionController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.Controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Joseph
+ */
+@Named(value = "loginActionController")
+@RequestScoped
+public class loginActionController {
+
+    /**
+     * Creates a new instance of loginActionController
+     */
+    public loginActionController() {
+    }
+    
+}
